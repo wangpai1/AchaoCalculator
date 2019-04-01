25d8f3c1fb450455f1d5ccf0cda82ac79e5ccc7f
“wangpaie <3465248225@qq.com>
2019-04-01 13:57:32 +0800

    First Commit

---
+++ wangpai1/四则运算/四则运算/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace Calculator
+{
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Random r = new Random();
+                int a = r.Next(0, 100);
+                int b = r.Next(0, 100);
+                int c = r.Next(0, 100);
+                string[] e = { "+", "-", "*", "/" };
+                char f, h;
+                Random g = new Random();
+                int ran = g.Next(0, 3);
+                int re = g.Next(0, 3);
+                f = char.Parse(e[ran % 4]);
+                h = char.Parse(e[re % 4]);
+                switch (f)
+                {
+                    case '+':
+                        switch (h)
+                        {
+                            case '+':
+                                Console.WriteLine(a + "+" + b + "+" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '-':
+                                Console.WriteLine(a + "+" + b + "-" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '*':
+                                Console.WriteLine(a + "+" + b + "*" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '/':
+                                Console.WriteLine(a + "+" + b + "/" + c + "=");
+                                Console.ReadKey();
+                                break;
+                        }
+                        break;
+                    case '-':
+                        switch (h)
+                        {
+                            case '+':
+                                Console.WriteLine(a + "-" + b + "+" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '-':
+                                Console.WriteLine(a + "-" + b + "-" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '*':
+                                Console.WriteLine(a + "-" + b + "*" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '/':
+                                Console.WriteLine(a + "-" + b + "/" + c + "=");
+                                Console.ReadKey();
+                                break;
+                        }
+                        break;
+                    case '*':
+                        switch (h)
+                        {
+                            case '+':
+                                Console.WriteLine(a + "*" + b + "+" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '-':
+                                Console.WriteLine(a + "*" + b + "-" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '*':
+                                Console.WriteLine(a + "*" + b + "*" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '/':
+                                Console.WriteLine(a + "*" + b + "/" + c + "=");
+                                Console.ReadKey();
+                                break;
+                        }
+                        break;
+                    case '/':
+                        switch (h)
+                        {
+                            case '+':
+                                Console.WriteLine(a + "/" + b + "+" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '-':
+                                Console.WriteLine(a + "/" + b + "-" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '*':
+                                Console.WriteLine(a + "/" + b + "*" + c + "=");
+                                Console.ReadKey();
+                                break;
+                            case '/':
+                                Console.WriteLine(a + "/" + b + "/" + c + "=");
+                                Console.ReadKey();
+                                break;
+                        }
+                        break;
+                }
+
+            }
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
